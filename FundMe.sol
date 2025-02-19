6400d4ea74dda6ca163eed5657a96c54aa670c1e
erickamaroamaro <erickamaroamaro@gmail.com>
2025-02-19 16:19:52 -0300

    building library, learn the safeMath

--- FundMe.sol
+++ FundMe.sol
@@ -2,40 +2,30 @@
 
 pragma solidity 0.8.19;
 
-interface AggregatorV3Interface {
-  function decimals() external view returns (uint8);
+import {PriceConverter} from "./PriceConverter.sol";
 
-  function description() external view returns (string memory);
-
-  function version() external view returns (uint256);
-
-  function getRoundData(
-    uint80 _roundId
-  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-
-  function latestRoundData()
-    external
-    view
-    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-}
 contract FundMe {
+    using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 5;
+    uint256 public minimumUsd = 5e18;
+
+    address[] public funders;
+    mapping (address funder => uint256 amountFunded) public addressToAmountFunded;
+
     // take a look at chainlink functions 
     function fund () payable public {
-        // allowing user to send money
-        // a mininum is required to send 
-        require(msg.value >= minimumUsd, "didn't send enoght eth");
+        require(msg.value.getConversionRate() >= minimumUsd, "didn't send enoght eth");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
         // revert transaction if condition not true
     }
 
-    function getPrice() public {
-        // address 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
-        // ABI 
-    }
-    function getConversionRate() public {}
+    function withdraw() public {
 
-    function getVersion() public view returns (uint256){
-        return AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419).version();
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
     }
+
 }

49f8a649c2c4f03044910daa00d7fcfa8b5fdba7
erickamaroamaro <erickamaroamaro@gmail.com>
2025-02-19 17:43:59 -0300

    Adding modifiers to our code, learning to use constant and immutable data, receive and fallback functions

--- FundMe.sol
+++ FundMe.sol
@@ -4,24 +4,31 @@ pragma solidity 0.8.19;
 
 import {PriceConverter} from "./PriceConverter.sol";
 
+error NotOwner();
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
 
     address[] public funders;
     mapping (address funder => uint256 amountFunded) public addressToAmountFunded;
 
+    address public immutable i_owner;
+
+    constructor(){
+        i_owner = msg.sender;
+    }
+
     // take a look at chainlink functions 
     function fund () payable public {
-        require(msg.value.getConversionRate() >= minimumUsd, "didn't send enoght eth");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "didn't send enoght eth");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
         // revert transaction if condition not true
     }
 
-    function withdraw() public {
-
+    function withdraw() public onlyOwner{
+       
         for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
@@ -34,8 +41,21 @@ contract FundMe {
         //bool sendSuccess = payable(msg.sender).send(address(this).balance);
         //require(sendSuccess, "Send fail");
         //call
-        //(bool callSuccess, ) = payable(msg.sender).call{value:address(this).balance}("");
-        //require(callSuccess, "call failed");
+        (bool callSuccess, ) = payable(msg.sender).call{value:address(this).balance}("");
+        require(callSuccess, "call failed");
     }
 
+    modifier onlyOwner(){
+        if(msg.sender != i_owner) { revert NotOwner();}
+         //require(msg.sender == i_owner,"Only owner can withdraw!");
+         _; //runs the following code
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
 }
